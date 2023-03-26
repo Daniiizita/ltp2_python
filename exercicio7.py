98ce1606da0e22b8eba0b07b669cd2bd958d549f
Danielle <goncalvesdani54@hotmail.com>
2023-03-26 19:39:33 -0300

    terminando os codigos que faltavam

--- exercicio7.py
+++ exercicio7.py
@@ -4,3 +4,10 @@
 
 
 """
+anos = int(input("Digite sua idade em anos: "))
+meses = int(input("Digite sua idade em anos: "))
+dias = int(input("Digite sua idade em dias: "))
+
+idade_em_dias = anos * 365 + meses * 30 + dias
+
+print(f"A idade em dias é {idade_em_dias} dias.")
